ebf8a9fbb5d0977fccdadd6b9d1e72250b5bf914
Glenn Smith <couleeapps@gmail.com>
2019-06-05 21:29:17 -0400

    Width field on InstructionTextToken

--- binaryninjacore.h
+++ binaryninjacore.h
@@ -1186,6 +1186,7 @@ extern "C"
 		BNInstructionTextTokenType type;
 		char* text;
 		uint64_t value;
+		uint64_t width;
 		size_t size, operand;
 		BNInstructionTextTokenContext context;
 		uint8_t confidence;
